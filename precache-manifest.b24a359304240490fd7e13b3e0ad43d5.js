e5810ea1786763d423ca045c1507e255a23a48e3
Oisin Kyne <kyneoisin@gmail.com>
2019-09-24 15:04:07 +0100

    Updates

---
+++ precache-manifest.b24a359304240490fd7e13b3e0ad43d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3fc3653ea6b61ee7420eeebc2c3c71",
+    "url": "/sibos-payidentity-frontend/index.html"
+  },
+  {
+    "revision": "2968b92a793875eee25e",
+    "url": "/sibos-payidentity-frontend/static/css/main.a8a1fc13.chunk.css"
+  },
+  {
+    "revision": "192c8b531e82084fd573",
+    "url": "/sibos-payidentity-frontend/static/js/2.255d1deb.chunk.js"
+  },
+  {
+    "revision": "2968b92a793875eee25e",
+    "url": "/sibos-payidentity-frontend/static/js/main.fe66dadf.chunk.js"
+  },
+  {
+    "revision": "b8e029ddf8b53949779c",
+    "url": "/sibos-payidentity-frontend/static/js/runtime~main.80210cf2.js"
+  }
+]);
