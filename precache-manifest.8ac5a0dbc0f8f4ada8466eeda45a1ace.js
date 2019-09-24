689ba87bac2b78403fea952fc681034b8e880d4e
Jaap Karan Singh <jksdua@gmail.com>
2019-09-24 16:28:23 +0100

    Updates

---
+++ precache-manifest.8ac5a0dbc0f8f4ada8466eeda45a1ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434389d7f837c8b1d9d9ae5cf7a64401",
+    "url": "/SibosYellowFrontEnd/index.html"
+  },
+  {
+    "revision": "1597656106b0c8f0d123",
+    "url": "/SibosYellowFrontEnd/static/css/main.a8a1fc13.chunk.css"
+  },
+  {
+    "revision": "3d598a36be35ebb09f55",
+    "url": "/SibosYellowFrontEnd/static/js/2.687f62e8.chunk.js"
+  },
+  {
+    "revision": "1597656106b0c8f0d123",
+    "url": "/SibosYellowFrontEnd/static/js/main.00814430.chunk.js"
+  },
+  {
+    "revision": "4d91f23b5b51c5bead70",
+    "url": "/SibosYellowFrontEnd/static/js/runtime~main.809e0f9c.js"
+  }
+]);
